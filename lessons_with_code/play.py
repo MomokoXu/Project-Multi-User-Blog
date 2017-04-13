4e4979b9615b95df1533611d8f523a31b233af04
MomokoXu <momoko0808@hotmail.com>
2017-04-12 22:57:49 -0700

    test different handlers

--- lessons_with_code/play.py
+++ lessons_with_code/play.py
@@ -6,13 +6,25 @@ form="""
 	<input type="submit">
 </form>
 """
+form2="""
+<form action="/testform">
+	<input name="q">
+	<input type="submit">
+</form>
+"""
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
         #self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write(form)
+        self.response.write(form2)
 
+class TestHandler(webapp2.RequestHandler):
+	def get(self):
+		#q = self.request.get("q")
+		#self.response.out.write(q)
+		self.response.headers['Content-Type'] = 'text/plain'
+		self.response.write(self.request)
 
 app = webapp2.WSGIApplication([
-    ('/', MainPage),
+    ('/', MainPage), ('/testform', TestHandler)
 ], debug=True)
