4d4616bb2df872b624e05099259ce9d39382f379
MomokoXu <momoko0808@hotmail.com>
2017-04-18 12:44:24 -0700

    create signup and welcome parts for blog

---
+++ lessons_with_code/blog/blog.py
@@ -0,0 +1,82 @@
+import os
+import webapp2
+import jinja2
+import re
+
+# os.path.join: concatenates two file names:
+    # os.path.dirname(__file__) directory of my current file is in
+    # 'templates'
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+# jinja_env: it means we render templates, jinja will look for those tmeplates in template_dir
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    # render: sends back to browser what render_str rendered
+    def render(self, template, **kw):
+        self.response.out.write(self.render_str(template, **kw))
+
+# Signup
+# 1. check regular expression matching for username, password and email
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    return username and USER_RE.match(username)
+
+PASS_RE = re.compile(r"^.{3,20}$")
+def valid_password(password):
+    return password and PASS_RE.match(password)
+
+EMAIL_RE = re.compile(r"^[\S]+@[\S]+.[\S]+$")
+def valid_email(email):
+    return not email or EMAIL_RE.match(email)
+
+# 2. Signup Handler
+class Signup(Handler):
+    def get(self):
+        self.render('signup.html')
+
+    def post(self):
+        username = self.request.get('username')
+        password = self.request.get('password')
+        email = self.request.get('email')
+        verify = self.request.get('verify')
+        have_error = False
+
+        params = dict(username = username, email = email)
+
+        if not valid_username(username):
+            params['username_error'] = "That's not a valid username."
+            have_error = True
+
+        if not valid_password(password):
+            params['password_error'] = "That's not a valid password."
+            have_error = True
+        elif password != verify:
+            params['verify_error'] = "Passwords not match."
+            have_error = True
+
+        if not valid_email(email):
+            params['email_error'] = "That's not a valid email address."
+            have_error = True
+
+        if have_error:
+            self.render('signup.html', **params)
+        else:
+            self.redirect('/welcome?username=' + username)
+
+# Welcome Handler
+class Welcome(Handler):
+    def get(self):
+        username = self.request.get('username')
+        if valid_username(username):
+            self.render('welcome.html', username = username)
+        else:
+            self.redirect('/signup')
+
+app = webapp2.WSGIApplication([('/signup', Signup), ('/welcome', Welcome)], debug=True)
